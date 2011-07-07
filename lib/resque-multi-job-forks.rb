547935e1c60b65b90e1c5c2093833b191002fd7a
John Le <john@doloreslabs.com>
2011-07-07 16:46:12 -0700

    updating stuff to exit the child process on finish and to signal the child process when the parent receives a signal

--- lib/resque-multi-job-forks.rb
+++ lib/resque-multi-job-forks.rb
@@ -7,6 +7,15 @@ module Resque
     attr_accessor :jobs_per_fork
     attr_reader   :jobs_processed
 
+    def send_child_process_signal(signal)
+      if @child
+        log! "Killing child at #{@child}"
+        if system("ps -o pid,state -p #{@child}")
+          Process.kill(signal, @child) rescue nil
+        end
+      end
+    end
+
     unless method_defined?(:shutdown_without_multi_job_forks)
       def perform_with_multi_job_forks(job = nil)
         perform_without_multi_job_forks(job)
@@ -16,19 +25,40 @@ module Resque
       alias_method :perform_without_multi_job_forks, :perform
       alias_method :perform, :perform_with_multi_job_forks
 
+      def shutdown_with_multi_job_forks?
+        # if this gets changed due to a signal handler we need to
+        # release the fork
+        #
+        # if the fork has been hijacked and we have reached our limit
+        # then we need to again release the fork
+        #
+        # if the fork is hijacked? when we are in the child process
+        release_fork if fork_hijacked? && (shutdown_without_multi_job_forks? || fork_job_limit_reached?)
+        shutdown_without_multi_job_forks?
+      end
+      alias_method :shutdown_without_multi_job_forks?, :shutdown?
+      alias_method :shutdown?, :shutdown_with_multi_job_forks?
+
       def shutdown_with_multi_job_forks
-        release_fork if fork_hijacked? && fork_job_limit_reached?
+        send_child_process_signal("QUIT")
         shutdown_without_multi_job_forks
       end
-      alias_method :shutdown_without_multi_job_forks, :shutdown?
-      alias_method :shutdown?, :shutdown_with_multi_job_forks
+      alias_method :shutdown_without_multi_job_forks, :shutdown
+      alias_method :shutdown, :shutdown_with_multi_job_forks
 
-      def working_on_with_worker_registration(job)
-        register_worker
-        working_on_without_worker_registration(job)
+      def pause_processing_with_multi_job_forks
+        send_child_process_signal("USR2")
+        pause_processing_without_multi_job_forks
       end
-      alias_method :working_on_without_worker_registration, :working_on
-      alias_method :working_on, :working_on_with_worker_registration
+      alias_method :pause_processing_without_multi_job_forks, :pause_processing
+      alias_method :pause_processing, :pause_processing_with_multi_job_forks
+
+      def unpause_processing_with_multi_job_forks
+        send_child_process_signal("CONT")
+        unpause_processing_without_multi_job_forks
+      end
+      alias_method :unpause_processing_without_multi_job_forks, :unpause_processing
+      alias_method :unpause_processing, :unpause_processing_with_multi_job_forks
     end
 
     def fork_hijacked?
@@ -51,6 +81,10 @@ module Resque
       @release_fork_limit = @jobs_processed = @cant_fork = nil
       log 'hijack over, counter terrorists win.'
       @shutdown = true unless $TESTING
+      # only the child calls release_fork, and it should exit and not
+      # passively shutdown otherwise it will call unregister worker
+      # after the work loop
+      exit!
     end
 
     def fork_job_limit
